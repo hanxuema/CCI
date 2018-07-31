ab058d329f5be9786db5a60d1c61c36715069c9c
Xavier Xie <xieyuehao@gmail.com>
2018-07-31 21:54:42 +1000

    add first version for CheckUniqueCharacterInString

--- CCI/Program.cs
+++ CCI/Program.cs
@@ -6,12 +6,37 @@ using System.Threading.Tasks;
 
 namespace CCI
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+        }
+
+        //1.1 in chapter 1, arrays and strings
+        public static bool CheckUniqueCharactersInString(string stringValue)
+        {
+            var str = stringValue.ToLower();
+            var result = true;
+            var _str = str;
+            for (int i = 0; i < str.Length; i++)
+            {
+                var count = 0;
+                for (int k = 0; k < _str.Length; k++)
+                {
+                    if (str[i].Equals(_str[k]))
+                    {
+                        count++;
+                    }
+                    if (count >= 2)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
---
+++ CCITests/ProgramTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCI.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CICISShouldReturnFalseIfStringIsAAA()
+        {
+            var result = Program.CheckUniqueCharactersInString("AAA");
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        public void CICISShouldReturnFalseIfStringIsABA()
+        {
+            var result = Program.CheckUniqueCharactersInString("ABA");
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        public void CICISShouldReturnTrueIfStringIsABC()
+        {
+            var result = Program.CheckUniqueCharactersInString("ABC");
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod()]
+        public void CICISShouldReturnTrueIfStringIsABCDEFGHIJK()
+        {
+            var result = Program.CheckUniqueCharactersInString("ABCDEFGHIJK");
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod()]
+        public void CICISShouldReturnFalseIfStringIsABCDEFGHIJKA()
+        {
+            var result = Program.CheckUniqueCharactersInString("ABCDEFGHIJKA");
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        public void CICISShouldReturnTrueIfStringIsABCDEFgHIJK()
+        {
+            var result = Program.CheckUniqueCharactersInString("ABCDEFgHIJK");
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod()]
+        public void CICISShouldReturnFalseIfStringIsABCDEFGHIJKa()
+        {
+            var result = Program.CheckUniqueCharactersInString("ABCDEFGHIJKa");
+
+            Assert.IsFalse(result);
+        }
+    }
+}
