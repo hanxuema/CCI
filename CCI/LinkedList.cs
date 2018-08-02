5b8a2228ceb18cec089931d2aa9d8edea642ecda
Xavier Xie <xieyuehao@gmail.com>
2018-08-03 00:16:08 +1000

    Add LinkedList Implementation

---
+++ CCI/LinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCI
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+
+        public LinkedListNode<T> Next { get; set; }
+
+    }
+
+    public class LinkedList<T> : System.Collections.Generic.ICollection<T>
+    {
+        public LinkedListNode<T> Head { get; private set; }
+        public LinkedListNode<T> Tail { get; private set; }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly { get; private set; }
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new LinkedListNode<T>(value));
+        }
+
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            //if there is a head
+            var temp = Head;
+            node.Next = temp;
+            Head = node;
+            Count++;
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new LinkedListNode<T>(value));
+        }
+
+        public void AddLast(LinkedListNode<T> node)
+        {
+            if (Count== 0)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                var temp = Tail;
+                Tail = node;
+                temp.Next = Tail;
+            }
+          
+            Count++;
+            if (Count == 1)
+            {
+                Head = Tail;
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            Tail = null;
+            if (Count == 1)
+            {
+                Head = null;
+            }
+            Count--;
+        }
+
+        public void RemoveLast()
+        {
+
+        }
+
+        public void PrintList(Node node)
+        {
+            while (node != null)
+            {
+
+            }
+        }
+
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CCITests/LinkedListTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCI.Tests
+{
+    [TestClass()]
+    public class LinkedListTests
+    {
+        [TestMethod()]
+        public void AddFirstTest()
+        {
+            var linkedList = new LinkedList<int>();
+            linkedList.AddFirst(1);
+            Assert.AreEqual(linkedList.Head.Value, 1);
+            Assert.AreEqual(linkedList.Head.Next, null);
+
+            Assert.AreEqual(linkedList.Tail, linkedList.Head);
+            Assert.AreEqual(linkedList.Count, 1);
+
+            linkedList.AddFirst(2);
+            Assert.AreEqual(linkedList.Head.Value, 2);
+            Assert.AreEqual(linkedList.Head.Next.Value, 1);
+
+            Assert.AreEqual(linkedList.Tail.Value, 1);
+            Assert.AreEqual(linkedList.Count, 2);
+
+        }
+
+        [TestMethod()]
+        public void AddLastTest()
+        {
+            var linkedList = new LinkedList<int>();
+            linkedList.AddLast(1);
+            Assert.AreEqual(linkedList.Head.Value, 1);
+            Assert.AreEqual(linkedList.Head.Next, null);
+
+            Assert.AreEqual(linkedList.Tail, linkedList.Head);
+            Assert.AreEqual(linkedList.Count, 1);
+
+            linkedList.AddLast(2);
+            Assert.AreEqual(linkedList.Head.Value, 1);
+            Assert.AreEqual(linkedList.Head.Next.Value, 2);
+
+            Assert.AreEqual(linkedList.Tail.Value, 2);
+            Assert.AreEqual(linkedList.Count, 2);
+
+        }
+
+        [TestMethod()]
+        public void RemoveFirstTest()
+        {
+            var linkedList = new LinkedList<int>();
+            linkedList.Add(1);
+            Assert.AreEqual(linkedList.Count, 1);
+            linkedList.Add(2);
+            Assert.AreEqual(linkedList.Count, 2);
+            linkedList.RemoveFirst();
+            Assert.AreEqual(linkedList.Count, 1);
+        }
+
+        [TestMethod()]
+        public void RemoveLastTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PrintListTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            var linkedList = new LinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Clear();
+            Assert.AreEqual(linkedList.Count, 0);
+
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CopyToTest()
+        {
+        
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+
+        }
+    }
+}
